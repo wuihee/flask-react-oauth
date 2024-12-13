c69079b1106870aa605d6736065bbfeeaf2af830
wuihee <wuihee@gmail.com>
2024-12-13 13:17:53 -0800

    Minor updates.

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,15 @@
-from flask import Blueprint
+from authlib.integrations.flask_client import OAuth
+from flask import Blueprint, redirect, url_for
 
 auth_blueprint = Blueprint("main", __name__)
+
+oauth = OAuth()
+oauth.register(
+    name="google",
+    access_token_url="https://accounts.google.com/o/oauth2/token",
+    authorize_url="https://accounts.google.com/o/oauth2/auth",
+    client_kwargs={"scope": "openid profile"},
+)
 
 
 @auth_blueprint.route("/api/ping")
@@ -10,12 +19,17 @@ def ping():
 
 @auth_blueprint.route("/login")
 def login():
-    pass
+    redirect_uri = url_for("authorize", _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
 
 
-@auth_blueprint.route("/callback")
-def oauth_callback():
-    pass
+@auth_blueprint.route("/authorize")
+def authorize():
+    token = oauth.google.authorize_access_token()
+    response = oauth.google.get("account/verify_credentials.json")
+    response.rase_for_status()
+    profile = response.json()
+    return redirect("/")
 
 
 @auth_blueprint.route("/logout")
